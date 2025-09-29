f0164d8885f7da90364c11dfda6ada6912a8454b
Raj Talvaniya <=>
2025-09-29 18:01:11 +0530

    .

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import Navbar from "../pages/HomePage/Navbar";
-import Footer from "../pages/HomePage/Footer";
-
+import React from 'react';
+import Navbar from '../pages/HomePage/Navbar';
+import Footer from '../pages/HomePage/Footer';
 
 const Layout = ({ children }) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className='flex flex-col min-h-screen'>
       <Navbar />
-      <main className="">{children}</main>
+      <main className='flex-1'>{children}</main>
       <Footer />
     </div>
   );
